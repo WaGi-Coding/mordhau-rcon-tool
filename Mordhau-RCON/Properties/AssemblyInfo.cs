d3f860d643ba4ea9b148870d8f3d0bac8e14e939
WaGi <wagi.coding@gmail.com>
2021-07-10 17:52:06 +0200

    1.0.4

--- Mordhau-RCON/Properties/AssemblyInfo.cs
+++ Mordhau-RCON/Properties/AssemblyInfo.cs
@@ -22,15 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4bf264d0-9c8c-4521-90f6-f5a1f4f84f73")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.4")]
 [assembly: AssemblyFileVersion("1.0.4")]
